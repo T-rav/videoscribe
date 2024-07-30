38b367947ce38164310248aa83f52968356c6f35
Travis Frisinger <tfrisinger@8thlight.com>
2024-07-30 15:18:23 -0600

    gen ai ui dev

--- src/ui/scribe/src/App.tsx
+++ src/ui/scribe/src/App.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Form from './components/Form/Form';
+import Footer from './components/Footer/Footer';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
-      {/* <div>
-     Transcribe YouTube Videos Effortlessly
-     Our SaaS platform makes it easy to convert your YouTube videos into text-based transcripts, captions, and subtitles.
-     </div> */}
-      <Form />
+      <div className="main-content">
+        <Form />
+      </div>
+      <Footer />
     </div>
   );
 }
