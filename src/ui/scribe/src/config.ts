ee91ad60244afa751e38f21ebe902fd6d9df48d0
Travis Frisinger <tfrisinger@8thlight.com>
2024-08-16 15:59:04 -0600

    got google login working

--- src/ui/scribe/src/config.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
--- src/ui/scribe/src/index.tsx
+++ src/ui/scribe/src/index.tsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { GOOGLE_CLIENT_ID } from './config';
+import { AuthProvider } from "./components/AuthContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
-  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID || ''}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ''}>
+    <AuthProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </AuthProvider>
   </GoogleOAuthProvider>
 );
