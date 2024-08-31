91bede2b0d73f6e55a39c95b1739acb85b6db9b2
Travis Frisinger <tmfrisinger@gmail.com>
2024-08-30 19:22:58 -0600

    got the size to come through and file content of function

--- src/translator/function/function_app.py
+++ src/translator/function/function_app.py
@@ -2,13 +2,17 @@ import azure.functions as func
 import datetime
 import json
 import logging
+import os  # Add this import to access environment variables
 
 app = func.FunctionApp()
 
+# Load the connection string from environment variables
+path = os.getenv("AZURE_STORAGE_PATH")
 
-@app.blob_trigger(arg_name="myblob", path="uploads",
-                               connection="BlobStorageConnectionString") 
-def ProcessTransribeRequest(myblob: func.InputStream):
+@app.blob_trigger(arg_name="request", path=path,
+                  connection="BLOB_STORAGE_CONNECTION_STRING")  # Update this line
+def ProcessTransribeRequest(request: func.InputStream):
     logging.info(f"Python blob trigger function processed blob"
-                f"Name: {myblob.name}"
-                f"Blob Size: {myblob.length} bytes")
+                f"Name: {request.name}")
+    content = request.read()
+    logging.info(f"Blob Size: {len(content)} bytes")
