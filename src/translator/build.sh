4a52157f7a17b8f71b5262b266bcdf6f6091e29f
Travis Frisinger <tfrisinger@8thlight.com>
2024-11-18 10:26:13 -0700

    removed some legacy docker stuff

--- src/translator/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker build -t scribe-ai-jobs-listener .
-
-docker run -d --name scribe-ai-jobs-listener -e RABBITMQ_URL=amqp://guest:guest@localhost:5672/ -e TRANSCRIPTION_QUEUE_NAME=transcription-jobs -e TRANSCRIPTION_QUEUE_NAME_DEMO=transcription-jobs-demo scribe-ai-jobs-listener scribe-ai-jobs-listener
